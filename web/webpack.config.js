7d24148f71f95fd995b4a8e922296c41f998168e
Dmytro Milinevskyi <dmilinevskyi@gmail.com>
2019-11-30 00:39:01 +0100

    web: support for .eng.ENV file for webpack builds

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,39 +1,58 @@
+const webpack = require('webpack');
+const dotenv = require('dotenv');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
+const fs = require('fs');
+const path = require('path');
+
+module.exports = (env) => {
+  const envFileName = '.env.' + env.ENVIRONMENT;
+  const envPath = path.join(__dirname) + '/' + envFileName;
+  if (!fs.existsSync(envPath)) {
+    throw new Error("Env file '" + envFileName + "' does not exist");
+  }
+  const envFile = dotenv.config({ path: envPath }).parsed;
+  const envKeys = Object.keys(envFile).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(envFile[next]);
+    return prev;
+  }, {});
+
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: "html-loader"
+            }
+          ]
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {
+              loader: "style-loader"
+            },
+            {
+              loader: "css-loader"
+            }
+          ]
         }
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
-          }
-        ]
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: "style-loader"
-          },
-          {
-            loader: "css-loader"
-          }
-        ]
-      }
+      ]
+    },
+    plugins: [
+      new webpack.DefinePlugin(envKeys),
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html"
+      })
     ]
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
-    })
-  ]
+  }
 };
