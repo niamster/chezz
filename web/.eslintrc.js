2428adf0877f752ddd1a5c2ed01387bfaf8e4663
Dmytro Milinevskyi <dmilinevskyi@gmail.com>
2019-12-02 07:49:19 +0100

    web: eslint config

---
+++ web/.eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    'semi': ['error', 'always'],
+    'indent': ['error', 2],
+    'space-before-function-paren': ['error', {
+      'anonymous': 'always',
+      'named': 'never',
+      'asyncArrow': 'always'
+    }],
+    'comma-dangle': ['error', 'only-multiline'],
+  }
+}
--- web/build.sh
+++ web/build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+eslint src webpack.config.js || exit 1
 npm run build
