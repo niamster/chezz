d7f6cedcc0a8a8722191f59ed99d282b62d9200d
Dmytro Milinevskyi <dmilinevskyi@gmail.com>
2019-12-04 20:58:18 +0100

    api:build: force google-java-format

--- api/build.sh
+++ api/build.sh
@@ -1,3 +1,9 @@
 #/usr/bin/env bash
 
+if ! ./gradlew verGJF; then
+  echo "==========================="
+  echo "run './gradlew goJF' to fix"
+  echo "==========================="
+  exit 1
+fi
 ./gradlew bootJar $@
--- api/run.sh
+++ api/run.sh
@@ -1,3 +1,9 @@
 #/usr/bin/env bash
 
+if ! ./gradlew verGJF; then
+  echo "==========================="
+  echo "run './gradlew goJF' to fix"
+  echo "==========================="
+  exit 1
+fi
 ./gradlew bootRun --args='--spring.config.location=file:./config/configuration.yaml' $@
