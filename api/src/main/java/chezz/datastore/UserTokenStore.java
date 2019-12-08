e75b1aa3147f44c9b84cb8b69870b063216205fa
Dmytro Milinevskyi <dmilinevskyi@gmail.com>
2019-12-08 23:07:34 +0100

    api: remove "public" modifiers from interface methods

--- api/src/main/java/chezz/datastore/UserTokenStore.java
+++ api/src/main/java/chezz/datastore/UserTokenStore.java
@@ -2,9 +2,9 @@ package chezz.datastore;
 
 public interface UserTokenStore {
 
-  public String getUser(String token);
+  String getUser(String token);
 
-  public void setUserToken(String username, String token);
+  void setUserToken(String username, String token);
 
-  public void removeToken(String token);
+  void removeToken(String token);
 }
