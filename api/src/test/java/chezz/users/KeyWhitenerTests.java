3f42bb4fa8c8668cf3adf6897905d8794730cde8
Dmytro Milinevskyi <dmilinevskyi@gmail.com>
2019-12-15 00:42:25 +0100

    api: uncrustify key whitener selector

--- api/src/test/java/chezz/users/KeyWhitenerTests.java
+++ api/src/test/java/chezz/users/KeyWhitenerTests.java
@@ -1,19 +1,27 @@
 package chezz.users;
 
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class KeyWhitenerTests {
 
+  @Test
+  public void testUnknownKeyWhitener() {
+    KeyWhitener kw = new KeyWhitenerSelector().getByName("???");
+    assertNull(kw);
+  }
+
   @DisplayName("Key Whitener tests")
   @ParameterizedTest(name = "run #{index} with [{arguments}]")
   @ValueSource(strings = {"bcrypt", "argon2"})
   public void testKeyWhitener(String name) {
-    KeyWhitener kw = new KeyWhitenerByName().getKeyWhitener(name);
+    KeyWhitener kw = new KeyWhitenerSelector().getByName(name);
     String hash_0 = kw.hash("key_0");
     String hash_1 = kw.hash("key_1");
     assertNotEquals(hash_0, hash_1);
