722cfaac52d1a6755741504bae24b15773f2817c
Dmytro Milinevskyi <dmilinevskyi@gmail.com>
2019-12-21 23:03:50 +0100

    api: add @Override everywhere

--- api/src/main/java/chezz/users/KeyWhitenerBCrypt.java
+++ api/src/main/java/chezz/users/KeyWhitenerBCrypt.java
@@ -4,10 +4,12 @@ import org.springframework.security.crypto.bcrypt.BCrypt;
 
 public class KeyWhitenerBCrypt implements KeyWhitener {
 
+  @Override
   public String hash(String key) {
     return BCrypt.hashpw(key, BCrypt.gensalt());
   }
 
+  @Override
   public boolean verify(String key, String hash) {
     return BCrypt.checkpw(key, hash);
   }
